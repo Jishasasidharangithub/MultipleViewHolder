35161cd97b861b2f80298b88ad1c6af5331a7ac4
jishameridian <jisha@meridian.net.in>
2023-05-23 10:32:23 +0530

    Order Info Completed

---
+++ app/src/main/java/com/template/tmp02/OrderInfoFragment.kt
@@ -0,0 +1,29 @@
+package com.template.tmp02
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.template.tmp02.databinding.FragmentOrderInfoBinding
+
+
+class OrderInfoFragment : Fragment() {
+  private var binding:FragmentOrderInfoBinding?=null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentOrderInfoBinding.inflate(inflater,container,false)
+         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+    private fun init(){
+        binding?.includeToolbar?.tvToolbarTitle?.text="ORDER INFO"
+    }
+
+ }
